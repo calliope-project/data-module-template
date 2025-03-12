1c6db5469d653ec63425c5e059fa09ae7bd1dc10
Ivan Ruiz Manuel <72193617+irm-codebase@users.noreply.github.com>
2025-03-12 12:19:11 +0100

    Add template (#1)
    * Initial template creation with pixi
    * Add standard testing and files
    * Add optional Read the Docs setup
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
    Co-authored-by: Stefan Pfenninger-Lee <stefan@pfenninger.org>

---
+++ tests/template_test.py
@@ -0,0 +1,40 @@
+"""Test a simplified template use-case."""
+
+import subprocess
+from copy import deepcopy
+
+import pytest
+from copier import run_copy
+
+
+@pytest.fixture(scope="module", params=["Apache-2.0"])
+def template_project(request, tmp_path_factory, template_path, simple_answers):
+    """Run the template in a temporary location, for further testing."""
+    path = tmp_path_factory.mktemp(request.param)
+    modified_answers = deepcopy(simple_answers)
+    modified_answers.update(license=request.param)
+    run_copy(src_path=str(template_path), dst_path=str(path), data=modified_answers)
+    return path / modified_answers["module_short_name"]
+
+
+@pytest.fixture(scope="module")
+def pixi_built(template_project):
+    """Create a pixi environment for the temporary template project."""
+    pixi_config = template_project / "pixi.toml"
+    subprocess.run(
+        f"pixi install --manifest-path {pixi_config}",
+        shell=True,
+        cwd=template_project,
+        check=True,
+    )
+    return template_project
+
+
+def test_mkdocs_build(pixi_built):
+    """The template's mkdocs should build without issues."""
+    assert subprocess.run("pixi run build-docs", shell=True, check=True, cwd=pixi_built)
+
+
+def test_pytest(pixi_built):
+    """The template's tests should pass by default."""
+    assert subprocess.run("pixi run test", shell=True, check=True, cwd=pixi_built)
